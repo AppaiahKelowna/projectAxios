3e9e6857fe2452104c298057c58daad41f699208
AppaiahKelowna <appaiahtwokelowna@gmail.com>
2025-08-28 11:17:39 -0700

    updated UI displaying employee data

--- App.tsx
+++ App.tsx
@@ -13,17 +13,26 @@ import {
   View,
   Text,
   TouchableOpacity,
+  FlatList,
+  TextInput,
 } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import './global.css';
-import { fetchEmployeeList } from './slice/employeeList';
-import { useDispatch } from 'react-redux';
+import {
+  fetchEmployeeList,
+  fetchEachEmployeeByID,
+  employeeData,
+  currentEmployeeData,
+} from './slice/employeeList';
+import { useDispatch, useSelector } from 'react-redux';
 import { Provider } from 'react-redux';
 import { Store } from '@reduxjs/toolkit';
 import { store } from './store/store';
+import { useEffect, useState } from 'react';
+import { employee } from './slice/employeeList';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -41,20 +50,77 @@ function App() {
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
   const dispatch = useDispatch();
+  const [userId, setUserID] = useState(1);
 
   const handleClick = () => {
-    console.log('calling fetchEmployeeList function');
     dispatch(fetchEmployeeList());
   };
+  const handleUser = () => {
+    dispatch(fetchEachEmployeeByID(userId));
+  };
+
+  const dataEmployeeList = useSelector(employeeData);
+  const dataCurrentEmployee = useSelector(currentEmployeeData);
+
+  const Item = ({ title }) => {
+    return (
+      <View>
+        <Text className="font-bold">{title}</Text>
+      </View>
+    );
+  };
+
+  const UserDetail = ({ user }: { user: employee }) => {
+    return (
+      <View>
+        <Text className="font-bold text-red-600">{user.name}</Text>
+        <Text className="font-bold  text-red-600">{user.phone}</Text>
+        <Text className="font-bold  text-red-600">{user.email}</Text>
+        <Text className="font-bold  text-red-600">{user.address?.city}</Text>
+        <Text className="font-bold  text-red-600">{user.address?.street}</Text>
+      </View>
+    );
+  };
+
+  useEffect(() => {}, []);
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Hello World!</Text>
-      <TouchableOpacity
-        onPress={handleClick}
-        className="bg-blue-600 p-4 border border-1 rounded-md"
-      >
-        <Text className="text-white font-bold">submit</Text>
-      </TouchableOpacity>
+    <View
+      className="flex-1 justify-start items-start gap-2 p-4 font-bold"
+      style={{ paddingTop: safeAreaInsets.top }}
+    >
+      <Text className="font-extrabold text-2xl">Employee List</Text>
+      <View className="flex-0">
+        <TouchableOpacity
+          onPress={handleClick}
+          className="bg-blue-600 p-4 border border-1 rounded-md"
+        >
+          <Text className="text-white font-bold">Fetch All Employees</Text>
+        </TouchableOpacity>
+        <FlatList
+          data={dataEmployeeList}
+          renderItem={({ item }) => <Item title={item.id + '. ' + item.name} />}
+          keyExtractor={item => item.id.toString()}
+          contentContainerStyle={{ flexGrow: 0 }}
+          style={{ flexGrow: 0 }}
+        />
+      </View>
+      <View className="flex-row gap-2">
+        <TouchableOpacity
+          onPress={handleUser}
+          className="bg-orange-600 p-4 border border-1 rounded-md"
+        >
+          <Text className="text-white font-bold">
+            Fetch selected user history
+          </Text>
+        </TouchableOpacity>
+        <TextInput
+          placeholder="Enter user id"
+          value={userId}
+          onChangeText={setUserID}
+          className="font-extrabold"
+        ></TextInput>
+      </View>
+      {dataCurrentEmployee && <UserDetail user={dataCurrentEmployee} />}
     </View>
   );
 }
--- slice/employeeList.ts
+++ slice/employeeList.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { store } from "../store/store";
 
-interface employee {
+export interface employee {
     id: number,
     name: string,
     username: string,
@@ -29,22 +29,46 @@ interface company {
     bs: string
 }
 
+const api = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com",
+    timeout: 5000,
+    headers: {'Content-Type': 'application/json'}
+})
+
 export const fetchEmployeeList = createAsyncThunk("employees/fetchEmployees", async () => {
     console.log("Inside fetchEmployeeList")
     try {
-        const response = await axios.get<employee[]>("https://jsonplaceholder.typicode.com/users")
-        if(response.data) {
-            console.log("Response Data", response.data)
-            return response.data
-        }
-        console.log("Response Data is empty", [])
-        return []
+        const response = await api.get<employee[]>("/users")
+         const data = response.data ? response.data : []
+         console.log("Response Data", data)
+         return data
     } catch (error) {
         console.log("api call Error", error)
+        return []
     }
 })
 
-const initialState : employee[] = []
+export const fetchEachEmployeeByID = createAsyncThunk("user/fetchEachUser", async (id) => {
+    try {
+        const response = await api.get<employee>(`/users/${id}`)
+        const data = response.data
+        console.log("Response Data", data)
+        return data
+    } catch (error) {
+        console.log("api call Error", error)
+        throw error
+    }
+})
+
+interface EmployeeState {
+    employeesList: employee[],
+    current: employee | null;
+}
+
+const initialState: EmployeeState = {
+    employeesList: [],
+    current: null
+}
 
 const employeeList = createSlice({
     name: "employeeList",
@@ -55,10 +79,15 @@ const employeeList = createSlice({
     extraReducers: (builder) => {
         builder
         .addCase(fetchEmployeeList.fulfilled, (state, action) => {
-           return action.payload ?? []
+           state.employeesList = action.payload
+        })
+        builder
+        .addCase(fetchEachEmployeeByID.fulfilled, (state, action) => {
+            state.current = action.payload
         })
     }
 })
 
 export default employeeList.reducer;
-export const employeeData = (state) => state.employeedata.values;
+export const employeeData = (state) => state.employeedata.employeesList;
+export const currentEmployeeData = (state) => state.employeedata.current;
